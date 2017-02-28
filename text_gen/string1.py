288525a917d6166cdbe8d3fdd32ac943bcffd231
mikekh84 <mikekh@uw.edu>
2017-02-28 13:55:26 -0800

    Project Started and generator that produces 3 random letters.

---
+++ text_gen/string1.py
@@ -0,0 +1,7 @@
+import string, random
+
+def generator():
+    letter1 = random.choice(string.ascii_letters)
+    letter2 = random.choice(string.ascii_letters)
+    letter3 = random.choice(string.ascii_letters)
+    
